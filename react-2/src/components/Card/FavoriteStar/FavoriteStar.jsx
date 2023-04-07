b8dd188c2c1caec542a45db13cf91645664d123a
Julia <vuntaj@gmail.com>
2023-04-08 02:51:03 +0300

    star

--- react-2/src/components/Card/FavoriteStar/FavoriteStar.jsx
+++ react-2/src/components/Card/FavoriteStar/FavoriteStar.jsx
@@ -1,16 +1,17 @@
 import { Component } from 'react'
+import { ReactComponent as StarIconBefore } from '../FavoriteStar/FavoriteStarBefore.svg'
+import { ReactComponent as StarIconAfter } from '../FavoriteStar/FavoriteStarAfter.svg'
 import './FavoriteStar.scss'
 import PropTypes from 'prop-types'
 
 class FavoriteStar extends Component {
     render() {
-        const { addFavorite, id, clicked } = this.props
+        const { addFavorite, id, clicked, backgroundColor } = this.props
         return (
             <div className='star'
                 id={id}
-                onClick={addFavorite}
-            >
-                {clicked ? '★' : '☆'}
+                onClick={addFavorite}>
+                {clicked ? <StarIconBefore /> : <StarIconAfter />}
             </div>
         )
     }
